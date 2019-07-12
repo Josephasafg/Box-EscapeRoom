15c1496e9cf1083c1112ee4399c0a2ede1548eaf
Joseph Asaf <ajgard7@gmail.com>
2019-07-12 13:28:49 +0300

    canvas added and working right

--- Utilities.py
+++ Utilities.py
@@ -1,8 +1,17 @@
-from tkinter import Frame, Canvas
+from tkinter import Frame, Canvas, Button
+from typing import List
 from tkinter import W, E, N, S
 
 
-def create_sub_canvas(parent_frame, r_span, c_span, border_color="red") -> Canvas:
+def create_clue_button_list(frame: Frame) -> List[Button]:
+    button_list = list()
+    for button in range(1, 4):
+        current_button = Button(frame, text=f"Clue #{button}")
+        button_list.append(current_button)
+    return button_list
+
+
+def create_sub_canvas(parent_frame: Frame, r_span: int, c_span: int, border_color="red") -> Canvas:
     canvas = Canvas(parent_frame, width=r_span, height=c_span, highlightbackground=border_color, highlightcolor=border_color,
                     highlightthickness=2, bg='black')
     # canvas.grid()
